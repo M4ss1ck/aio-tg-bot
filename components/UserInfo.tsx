3157c393be01c34ae762e71b3191ca264e7e61f1
Massick <4ndyraul@gmail.com>
2024-09-28 02:49:30 -0300

    fix build errors

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -3,8 +3,20 @@
 import { useState, useEffect } from 'react'
 
 export const UserInfo = () => {
-    const userData = window.Telegram.WebApp.initData
-    console.log(userData)
+    const [userData, setUserData] = useState<string>("");
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const initData = decodeURIComponent(window.Telegram.WebApp.initData).split("&");
+            const userDataFiltered = initData.map(pair => pair.split("="));
+            for (const pair of userDataFiltered) {
+                if (pair[0] === 'user') {
+                    setUserData(pair[1]);
+                }
+
+            }
+        }
+    }, []);
     return <>
         <pre>{userData}</pre>
     </>
