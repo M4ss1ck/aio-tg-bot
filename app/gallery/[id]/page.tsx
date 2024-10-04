9d3a49abd0d60e03ccaeea97bf5231499bb8a9f6
Massick <4ndyraul@gmail.com>
2024-10-04 00:23:54 -0300

    add Personal Gallery component and page

---
+++ app/gallery/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { prisma } from '../../../db/prisma'
+import { pathToTgLink } from '../../../utils/functions'
+import { Gallery } from '../../../components/Gallery'
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const photos = await prisma.photo.findMany({
+        where: {
+            userId: params.id
+        }
+    })
+    if (!photos || photos.length === 0) return (<main className="flex flex-col items-center justify-center w-full h-full min-h-screen">
+        <h1 className="p-4 text-4xl">Your Personal Gallery is Empty</h1>
+        <section className="flex flex-col items-center justify-center gap-2 p-6 my-6">
+            <p className="text-xl">But you can now create it using <code>/gallery</code> command replying to photos</p>
+            <p className="text-lg">Try it out!</p>
+        </section>
+    </main>)
+
+    const photosWithLink = await Promise.all(photos.map(async photo => {
+        const response = await fetch(pathToTgLink(photo.path, photo.token))
+        const buffer = await response.arrayBuffer()
+        const base64Flag = 'data:image/jpeg;base64,'
+        const imageStr = arrayBufferToBase64(buffer)
+        return {
+            ...photo,
+            src: base64Flag + imageStr
+        }
+    }))
+
+    function arrayBufferToBase64(buffer: ArrayBuffer) {
+        let binary = ''
+        const bytes = new Uint8Array(buffer)
+        const len = bytes.byteLength
+        for (let i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i])
+        }
+        return window.btoa(binary)
+    }
+
+    return (
+        <main>
+            <Gallery photos={photosWithLink} />
+        </main>
+    )
+}
---
+++ components/Gallery.tsx
@@ -0,0 +1,16 @@
+
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+
+interface GalleryProps {
+    photos: {
+        src: string
+        [key: string]: any
+    }[];
+}
+
+export const Gallery = ({ photos }: GalleryProps) => {
+    return (
+        <Carousel images={photos} />
+    )
+}
