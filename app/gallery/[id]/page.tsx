8aeb34934e7634a3c169f966e8be12d1a32d9c7f
Massick <4ndyraul@gmail.com>
2024-10-04 01:07:56 -0300

    fix arrayBufferToBase64 for NodeJS

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -28,13 +28,8 @@ export default async function Page({ params }: { params: { id: string } }) {
     }))
 
     function arrayBufferToBase64(buffer: ArrayBuffer) {
-        let binary = ''
-        const bytes = new Uint8Array(buffer)
-        const len = bytes.byteLength
-        for (let i = 0; i < len; i++) {
-            binary += String.fromCharCode(bytes[i])
-        }
-        return window.btoa(binary)
+        const binary = Buffer.from(buffer).toString('base64')
+        return binary
     }
 
     return (
