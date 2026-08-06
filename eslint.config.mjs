0c6a4078d571d81e98ae608b756dcaa983d59472
Andy Palmero <4ndyraul@gmail.com>
2026-08-05 22:44:02 -0400

    chore(lint): migrate to ESLint 9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+export default defineConfig([
+    {
+        ignores: [
+            ".next/**",
+            "node_modules/**",
+            "public/**",
+            "docs/**",
+            "next-env.d.ts",
+        ],
+    },
+    {
+        extends: [...nextCoreWebVitals, ...nextTypescript],
+    },
+]);
